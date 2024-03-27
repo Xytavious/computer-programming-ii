c5346bb658c8263b556d62b0b2d64013d56a01c0
Xytavious <143834355+Xytavious@users.noreply.github.com>
2024-03-27 14:39:35 -0500

    3/27

--- longDistance/longDistance/MainForm.py
+++ longDistance/longDistance/MainForm.py
@@ -9,9 +9,86 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._label1 = System.Windows.Forms.Label()
+		self._label2 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(12, 226)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Location = System.Drawing.Point(103, 226)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(75, 23)
+		self._button2.TabIndex = 1
+		self._button2.Text = "button2"
+		self._button2.UseVisualStyleBackColor = True
+		# 
+		# button3
+		# 
+		self._button3.Location = System.Drawing.Point(202, 226)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(75, 23)
+		self._button3.TabIndex = 2
+		self._button3.Text = "button3"
+		self._button3.UseVisualStyleBackColor = True
+		# 
+		# textBox1
+		# 
+		self._textBox1.Location = System.Drawing.Point(156, 27)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(100, 20)
+		self._textBox1.TabIndex = 3
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.SystemColors.HighlightText
+		self._label1.Location = System.Drawing.Point(12, 67)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(119, 114)
+		self._label1.TabIndex = 4
+		# 
+		# label2
+		# 
+		self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+		self._label2.Location = System.Drawing.Point(86, 24)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(64, 23)
+		self._label2.TabIndex = 5
+		self._label2.Text = "Minutes:"
 		# 
 		# MainForm
 		# 
+		self.BackColor = System.Drawing.SystemColors.HotTrack
+		self.ClientSize = System.Drawing.Size(284, 261)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
 		self.Name = "MainForm"
 		self.Text = "longDistance"
+		self.ResumeLayout(False)
+		self.PerformLayout()
 
+
+	def Button1Click(self, sender, e):
+		def __
+		
+		eve
+		peak
+		
