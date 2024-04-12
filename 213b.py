e9ec59e3982be3dc26786691959b7dfcd620ae6c
Xytavious <143834355+Xytavious@users.noreply.github.com>
2024-04-12 19:20:26 +0000

    213b Complete 4/12/2024

---
+++ 213b.py
@@ -0,0 +1,111 @@
+q1 = int(input("Enter the Quantity: "))
+
+
+price = 0
+am = price * q1
+if 0 < q1 <= 99:
+  price = 5.95
+  am = price * q1
+  print("Amount Due: ", am)
+
+elif 100 <= q1 <= 199:
+  price = 5.75
+  am = price * q1
+  print("Amount Due: ", am)
+
+elif 200 <= q1 <= 299:
+  price = 5.40
+  am = price * q1
+  print("Amount Due: ", am)
+
+elif 300 <= q1 > 10000000:
+  price = 5.15
+  am = price * q1
+  print("Amount Due: ", am)
+
+
+print(price)
+
+q2 = int(input("Enter the Quantity: "))
+
+
+price = 0
+am = price * q2
+if 0 < q2 <= 99:
+  price = 5.95
+  am = price * q2
+  print("Amount Due: ", am)
+
+elif 100 <= q2 <= 199:
+  price = 5.75
+  am = price * q2
+  print("Amount Due: ", am)
+
+elif 200 <= q2 <= 299:
+  price = 5.40
+  am = price * q2
+  print("Amount Due: ", am)
+
+elif 300 <= q2 > 10000000:
+  price = 5.15
+  am = price * q2
+  print("Amount Due: ", am)
+
+
+print(price)
+
+q3 = int(input("Enter the Quantity: "))
+
+
+price = 0
+am = price * q3
+if 0 < q3 <= 99:
+  price = 5.95
+  am = price * q3
+  print("Amount Due: ", am)
+
+elif 100 <= q3 <= 199:
+  price = 5.75
+  am = price * q3
+  print("Amount Due: ", am)
+
+elif 200 <= q3 <= 299:
+  price = 5.40
+  am = price * q3
+  print("Amount Due: ", am)
+
+elif 300 <= q3 > 10000000:
+  price = 5.15
+  am = price * q3
+  print("Amount Due: ", am)
+
+
+print(price)
+
+q4 = int(input("Enter the Quantity: "))
+
+
+price = 0
+am = price * q4
+if 0 < q4 <= 99:
+  price = 5.95
+  am = price * q4
+  print("Amount Due: ", am)
+
+elif 100 <= q4 <= 199:
+  price = 5.75
+  am = price * q4
+  print("Amount Due: ", am)
+
+elif 200 <= q4 <= 299:
+  price = 5.40
+  am = price * q4
+  print("Amount Due: ", am)
+
+elif 300 <= q4 < 10000000:
+  price = 5.15
+  am = price * q4
+  print("Amount Due: ", am)
+
+
+print(price)
