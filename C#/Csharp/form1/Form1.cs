5937ce06aed8c4af28cf8645016fd6feb8c6b410
Xytavious <143834355+Xytavious@users.noreply.github.com>
2024-04-16 14:37:50 -0500

    up

--- C#/Csharp/form1/Form1.cs
+++ C#/Csharp/form1/Form1.cs
@@ -16,5 +16,45 @@ namespace form1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int sum = num1 + num2;
+            int diff = num1 - num2;
+            int prod = num1 * num2;
+
+            double avg = (double)sum / 2.0;
+            int abs = Math.Abs(diff);
+            // Math.Max and Math.Min
+            int max = 0;
+            int min = 0;
+
+            if (num1 > num2)
+                max = num1;
+            else
+                max = num2;
+
+
+            if (num1 <= num2)
+                min = num1;
+            else min = num2;
+            // else if 
+
+            lblSum.Text = sum.ToString();
+            lblMax.Text = max.ToString();
+            lblMin.Text = min.ToString();
+            lblMin.Text = min.ToString();
+            lbl.Text = min.ToString();
+            lbl.Text = min.ToString();
+            lbl.Text = min.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
